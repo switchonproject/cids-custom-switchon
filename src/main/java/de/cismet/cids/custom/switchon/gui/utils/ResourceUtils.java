d63de8b17b2b0cb76bbd36cbdf1333812faf2c4f
Gilles Baatz <gilles.baatz@cismet.de>
2014-07-28 12:13:33 +0200

    #14 created class ResourceUtils
    added a static method to filter tags with a certain taggroup of a resource bean

---
+++ src/main/java/de/cismet/cids/custom/switchon/gui/utils/ResourceUtils.java
@@ -0,0 +1,44 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.switchon.gui.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.cismet.cids.dynamics.CidsBean;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   Gilles Baatz
+ * @version  $Revision$, $Date$
+ */
+public class ResourceUtils {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   resourceBean  DOCUMENT ME!
+     * @param   taggroup      DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public static List<CidsBean> filterTagsOfResource(final CidsBean resourceBean, final Taggroups taggroup) {
+        final List<CidsBean> tags = resourceBean.getBeanCollectionProperty("tags");
+        final List<CidsBean> filteredTags = new ArrayList<CidsBean>();
+        for (final CidsBean tag : tags) {
+            final String taggroupName = (String)tag.getProperty("taggroup.name");
+            if (taggroup.getValue().equalsIgnoreCase(taggroupName)) {
+                filteredTags.add(tag);
+            }
+        }
+        return filteredTags;
+    }
+}
