f147948b3829f98ae045bc53feddf5de658c5191
Gilles Baatz <gilles.baatz@cismet.de>
2014-08-28 13:32:13 +0200

    #55 panel listens to changes in CidsBean
    The BasicResourcePropertiesPanel listens now to changes in its resource
    cidsBean.

--- src/main/java/de/cismet/cids/custom/switchon/wizards/panels/BasicResourcePropertiesPanel.java
+++ src/main/java/de/cismet/cids/custom/switchon/wizards/panels/BasicResourcePropertiesPanel.java
@@ -13,6 +13,9 @@ import org.openide.WizardDescriptor;
 
 import java.awt.Component;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import de.cismet.cids.custom.switchon.wizards.MetaDataWizardAction;
 import de.cismet.cids.custom.switchon.wizards.NameProvider;
 
@@ -26,7 +29,7 @@ import de.cismet.commons.gui.wizard.AbstractWizardPanel;
  * @author   Gilles Baatz
  * @version  $Revision$, $Date$
  */
-public class BasicResourcePropertiesPanel extends AbstractWizardPanel implements NameProvider {
+public class BasicResourcePropertiesPanel extends AbstractWizardPanel implements NameProvider, PropertyChangeListener {
 
     //~ Static fields/initializers ---------------------------------------------
 
@@ -42,6 +45,7 @@ public class BasicResourcePropertiesPanel extends AbstractWizardPanel implements
     @Override
     protected void read(final WizardDescriptor wizard) {
         final CidsBean resource = (CidsBean)wizard.getProperty(MetaDataWizardAction.PROP_RESOURCE_BEAN);
+        resource.addPropertyChangeListener(this);
         ((BasicResourcePropertiesVisualPanel)getComponent()).setCidsBean(resource);
     }
 
@@ -49,11 +53,17 @@ public class BasicResourcePropertiesPanel extends AbstractWizardPanel implements
     protected void store(final WizardDescriptor wizard) {
         final CidsBean resource = ((BasicResourcePropertiesVisualPanel)getComponent()).getCidsBean();
         wizard.putProperty(MetaDataWizardAction.PROP_RESOURCE_BEAN, resource);
+        resource.removePropertyChangeListener(this);
     }
 
     @Override
     public String getName() {
         return java.util.ResourceBundle.getBundle("de/cismet/cids/custom/switchon/wizards/Bundle")
                     .getString("BasicResourcePropertiesPanel.getName()");
+    }
+
+    @Override
+    public void propertyChange(final PropertyChangeEvent evt) {
+        changeSupport.fireChange();
     }
 }
