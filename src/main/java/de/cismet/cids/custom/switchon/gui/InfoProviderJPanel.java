e9a89bde177ed023643726c64fd2aa222ef3558c
Gilles Baatz <gilles.baatz@cismet.de>
2014-08-28 11:12:14 +0200

    #55 created the class InfoProviderJPanel
    InfoProviderJPanel is a class which is a JPanel implementing the
    interface InfoProvider. This interface can set information into a
    InfoReceiver.

---
+++ src/main/java/de/cismet/cids/custom/switchon/gui/InfoProviderJPanel.java
@@ -0,0 +1,47 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.switchon.gui;
+
+import javax.swing.JPanel;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   Gilles Baatz
+ * @version  $Revision$, $Date$
+ */
+public class InfoProviderJPanel extends JPanel implements InfoProvider {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final transient org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(
+            InfoProviderJPanel.class);
+
+    //~ Instance fields --------------------------------------------------------
+
+    private InfoReceiver infoReceiver;
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public void provideInformation(final String information) {
+        if (infoReceiver != null) {
+            infoReceiver.setInformation(information);
+        }
+    }
+
+    @Override
+    public InfoReceiver getInfoReceiver() {
+        return infoReceiver;
+    }
+
+    @Override
+    public void setInfoReceiver(final InfoReceiver infoReceiver) {
+        this.infoReceiver = infoReceiver;
+    }
+}
