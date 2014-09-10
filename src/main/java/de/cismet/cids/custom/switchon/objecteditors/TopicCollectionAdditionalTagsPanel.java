e337c94c6dd59a7de851f9cac3b2a4398ed72d2b
Gilles Baatz <gilles.baatz@cismet.de>
2014-09-10 12:03:54 +0200

    #81 added new collection button
    Added a new collection button to TopicCollectionAdditionalTagsPanel.
    A CreateNewTagAction-action was added to this button.

--- src/main/java/de/cismet/cids/custom/switchon/objecteditors/TopicCollectionAdditionalTagsPanel.java
+++ src/main/java/de/cismet/cids/custom/switchon/objecteditors/TopicCollectionAdditionalTagsPanel.java
@@ -40,8 +40,10 @@ public class TopicCollectionAdditionalTagsPanel extends InfoProviderJPanel imple
     private CidsBean cidsBean;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private de.cismet.cids.custom.switchon.objecteditors.AdditionalTagsPanel additionalTagsPanel;
+    private javax.swing.JButton btnNewCollection;
     private javax.swing.JComboBox cmbCollection;
     private javax.swing.JComboBox cmbTopic;
+    private de.cismet.cids.custom.switchon.gui.utils.CreateNewTagAction createNewCollection;
     private javax.swing.JPanel pnlCollection;
     private javax.swing.JPanel pnlTopicCategory;
     private org.jdesktop.beansbinding.BindingGroup bindingGroup;
@@ -68,11 +70,14 @@ public class TopicCollectionAdditionalTagsPanel extends InfoProviderJPanel imple
         java.awt.GridBagConstraints gridBagConstraints;
         bindingGroup = new org.jdesktop.beansbinding.BindingGroup();
 
+        createNewCollection = new de.cismet.cids.custom.switchon.gui.utils.CreateNewTagAction();
         pnlTopicCategory = new javax.swing.JPanel();
-        cmbTopic = FastBindableReferenceComboFactory.createTagsFastBindableReferenceComboBox(Taggroups.TOPIC_CATEGORY);
+        cmbTopic = FastBindableReferenceComboFactory.createTagsFastBindableReferenceComboBox(Taggroups.TOPIC_CATEGORY)
+        ;
         pnlCollection = new javax.swing.JPanel();
-        cmbCollection = FastBindableReferenceComboFactory.createTagsFastBindableReferenceComboBox(Taggroups.COLLECTION);
-        final ArrayList<Taggroups> taggroups = new ArrayList<Taggroups>();
+        cmbCollection = FastBindableReferenceComboFactory.createTagsFastBindableReferenceComboBox(Taggroups.COLLECTION) ;
+        btnNewCollection = new javax.swing.JButton();
+        ArrayList<Taggroups> taggroups = new ArrayList<Taggroups>();
         taggroups.add(Taggroups.COLLECTION);
         taggroups.add(Taggroups.GEOGRAPHY);
         taggroups.add(Taggroups.HYDROLOGICAL_CONCEPT);
@@ -82,27 +87,17 @@ public class TopicCollectionAdditionalTagsPanel extends InfoProviderJPanel imple
 
         setLayout(new java.awt.GridBagLayout());
 
-        pnlTopicCategory.setBorder(javax.swing.BorderFactory.createTitledBorder(
-                org.openide.util.NbBundle.getMessage(
-                    TopicCollectionAdditionalTagsPanel.class,
-                    "TopicCollectionAdditionalTagsPanel.pnlTopicCategory.border.title"))); // NOI18N
+        pnlTopicCategory.setBorder(javax.swing.BorderFactory.createTitledBorder(org.openide.util.NbBundle.getMessage(TopicCollectionAdditionalTagsPanel.class, "TopicCollectionAdditionalTagsPanel.pnlTopicCategory.border.title"))); // NOI18N
         pnlTopicCategory.setLayout(new java.awt.GridBagLayout());
 
-        org.jdesktop.beansbinding.Binding binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(
-                org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE,
-                this,
-                org.jdesktop.beansbinding.ELProperty.create("${cidsBean.topiccategory}"),
-                cmbTopic,
-                org.jdesktop.beansbinding.BeanProperty.create("selectedItem"));
+        org.jdesktop.beansbinding.Binding binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, this, org.jdesktop.beansbinding.ELProperty.create("${cidsBean.topiccategory}"), cmbTopic, org.jdesktop.beansbinding.BeanProperty.create("selectedItem"));
         bindingGroup.addBinding(binding);
 
         cmbTopic.addActionListener(new java.awt.event.ActionListener() {
-
-                @Override
-                public void actionPerformed(final java.awt.event.ActionEvent evt) {
-                    cmbTopicActionPerformed(evt);
-                }
-            });
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmbTopicActionPerformed(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.weightx = 1.0;
@@ -117,27 +112,17 @@ public class TopicCollectionAdditionalTagsPanel extends InfoProviderJPanel imple
         gridBagConstraints.insets = new java.awt.Insets(10, 10, 0, 10);
         add(pnlTopicCategory, gridBagConstraints);
 
-        pnlCollection.setBorder(javax.swing.BorderFactory.createTitledBorder(
-                org.openide.util.NbBundle.getMessage(
-                    TopicCollectionAdditionalTagsPanel.class,
-                    "TopicCollectionAdditionalTagsPanel.pnlCollection.border.title"))); // NOI18N
+        pnlCollection.setBorder(javax.swing.BorderFactory.createTitledBorder(org.openide.util.NbBundle.getMessage(TopicCollectionAdditionalTagsPanel.class, "TopicCollectionAdditionalTagsPanel.pnlCollection.border.title"))); // NOI18N
         pnlCollection.setLayout(new java.awt.GridBagLayout());
 
-        binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(
-                org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE,
-                this,
-                org.jdesktop.beansbinding.ELProperty.create("${cidsBean.collection}"),
-                cmbCollection,
-                org.jdesktop.beansbinding.BeanProperty.create("selectedItem"));
+        binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, this, org.jdesktop.beansbinding.ELProperty.create("${cidsBean.collection}"), cmbCollection, org.jdesktop.beansbinding.BeanProperty.create("selectedItem"));
         bindingGroup.addBinding(binding);
 
         cmbCollection.addActionListener(new java.awt.event.ActionListener() {
-
-                @Override
-                public void actionPerformed(final java.awt.event.ActionEvent evt) {
-                    cmbCollectionActionPerformed(evt);
-                }
-            });
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmbCollectionActionPerformed(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
@@ -145,6 +130,14 @@ public class TopicCollectionAdditionalTagsPanel extends InfoProviderJPanel imple
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.insets = new java.awt.Insets(10, 10, 10, 10);
         pnlCollection.add(cmbCollection, gridBagConstraints);
+        createNewCollection.setCombo((FastBindableReferenceCombo)cmbCollection);
+
+        org.openide.awt.Mnemonics.setLocalizedText(btnNewCollection, org.openide.util.NbBundle.getMessage(TopicCollectionAdditionalTagsPanel.class, "TopicCollectionAdditionalTagsPanel.btnNewCollection.text")); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.insets = new java.awt.Insets(10, 0, 10, 10);
+        pnlCollection.add(btnNewCollection, gridBagConstraints);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
@@ -162,27 +155,27 @@ public class TopicCollectionAdditionalTagsPanel extends InfoProviderJPanel imple
         add(additionalTagsPanel, gridBagConstraints);
 
         bindingGroup.bind();
-    } // </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>//GEN-END:initComponents
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cmbTopicActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cmbTopicActionPerformed
+    private void cmbTopicActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbTopicActionPerformed
         final String desc = TagUtils.getDescriptionOfTag(cmbTopic.getSelectedItem());
         provideInformation(desc);
-    }                                                                            //GEN-LAST:event_cmbTopicActionPerformed
+    }//GEN-LAST:event_cmbTopicActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cmbCollectionActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cmbCollectionActionPerformed
+    private void cmbCollectionActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbCollectionActionPerformed
         final String desc = TagUtils.getDescriptionOfTag(cmbCollection.getSelectedItem());
         provideInformation(desc);
-    }                                                                                 //GEN-LAST:event_cmbCollectionActionPerformed
+    }//GEN-LAST:event_cmbCollectionActionPerformed
 
     @Override
     public CidsBean getCidsBean() {
