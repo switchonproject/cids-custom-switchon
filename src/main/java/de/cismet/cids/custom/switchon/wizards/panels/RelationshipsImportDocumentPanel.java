9a0d3c1ca46f85e036cbe37adb1392238a4065ff
Gilles Baatz <gilles.baatz@cismet.de>
2014-09-22 14:50:18 +0200

    #88 configured RelationshipsImportDocumentPanel

--- src/main/java/de/cismet/cids/custom/switchon/wizards/panels/RelationshipsImportDocumentPanel.java
+++ src/main/java/de/cismet/cids/custom/switchon/wizards/panels/RelationshipsImportDocumentPanel.java
@@ -10,12 +10,16 @@ package de.cismet.cids.custom.switchon.wizards.panels;
 import org.apache.log4j.Logger;
 
 import org.openide.WizardDescriptor;
+import org.openide.util.Exceptions;
 
-import java.awt.Component;
+import java.util.List;
 
+import de.cismet.cids.custom.switchon.wizards.DefaultPropertySetter;
+import de.cismet.cids.custom.switchon.wizards.GenericAbstractWizardPanel;
+import de.cismet.cids.custom.switchon.wizards.MetaDataWizardAction;
 import de.cismet.cids.custom.switchon.wizards.NameProvider;
 
-import de.cismet.commons.gui.wizard.AbstractWizardPanel;
+import de.cismet.cids.dynamics.CidsBean;
 
 /**
  * DOCUMENT ME!
@@ -23,7 +27,8 @@ import de.cismet.commons.gui.wizard.AbstractWizardPanel;
  * @author   Gilles Baatz
  * @version  $Revision$, $Date$
  */
-public class RelationshipsImportDocumentPanel extends AbstractWizardPanel implements NameProvider {
+public class RelationshipsImportDocumentPanel extends GenericAbstractWizardPanel<RelationshipsImportDocumentVisualPanel>
+        implements NameProvider {
 
     //~ Static fields/initializers ---------------------------------------------
 
@@ -35,18 +40,24 @@ public class RelationshipsImportDocumentPanel extends AbstractWizardPanel implem
      * Creates a new RelationshipsImportDocumentPanel object.
      */
     public RelationshipsImportDocumentPanel() {
+        super(RelationshipsImportDocumentVisualPanel.class);
     }
 
     //~ Methods ----------------------------------------------------------------
 
     @Override
-    protected Component createComponent() {
-        return new RelationshipsImportDocumentVisualPanel();
-    }
-
-    @Override
     protected void read(final WizardDescriptor wizard) {
-        LOG.fatal("RelationshipsImportDocumentPanel.read: Not supported yet.", new Exception()); // NOI18N
+        final CidsBean relationship = (CidsBean)wizard.getProperty(MetaDataWizardAction.PROP_CREATED_RELATIONSHIP_BEAN);
+        final List<CidsBean> metaDatas = relationship.getBeanCollectionProperty("metadata");
+        if (metaDatas.isEmpty()) {
+            final CidsBean newMetaData = createNewMetaDataCidsBean(relationship);
+            if (newMetaData != null) {
+                metaDatas.add(newMetaData);
+            }
+        }
+        if (!metaDatas.isEmpty()) {
+            getComponent().setCidsBean(metaDatas.get(0));
+        }
     }
 
     @Override
@@ -59,5 +70,29 @@ public class RelationshipsImportDocumentPanel extends AbstractWizardPanel implem
         return org.openide.util.NbBundle.getMessage(
                 RelationshipsImportDocumentPanel.class,
                 "RelationshipsImportDocumentPanel.name");
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   relationship  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private CidsBean createNewMetaDataCidsBean(final CidsBean relationship) {
+        final CidsBean metaData;
+        try {
+            metaData = CidsBean.createNewCidsBeanFromTableName("SWITCHON", "metadata");
+            DefaultPropertySetter.setDefaultsToRelationshipMetaDataCidsBean(metaData);
+            metaData.setProperty("name", relationship.toString() + " Meta-Data");
+            metaData.setProperty("description", "Meta-Data of the " + relationship.toString());
+            final CidsBean contact = CidsBean.createNewCidsBeanFromTableName("SWITCHON", "contact");
+            DefaultPropertySetter.setDefaultsToMetaDataContactCidsBean(contact);
+            metaData.setProperty("contact", contact);
+            return metaData;
+        } catch (Exception ex) {
+            LOG.error(ex, ex);
+        }
+        return null;
     }
 }
