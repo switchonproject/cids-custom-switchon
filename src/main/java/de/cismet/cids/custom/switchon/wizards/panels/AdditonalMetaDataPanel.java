c9cc68420f73e7daf0633c2f83483f641de97b2e
Gilles Baatz <gilles.baatz@cismet.de>
2014-09-26 11:33:38 +0200

    #97 do nothing if previous button was pressed
    In AdditonalMetaDataPanel and RepresentationsPanel if store() is called,
    only create a new CidsBean or edit another CidsBean, if the Next-button
    was pressed.

--- src/main/java/de/cismet/cids/custom/switchon/wizards/panels/AdditonalMetaDataPanel.java
+++ src/main/java/de/cismet/cids/custom/switchon/wizards/panels/AdditonalMetaDataPanel.java
@@ -78,33 +78,36 @@ public class AdditonalMetaDataPanel extends GenericAbstractWizardPanel<Additonal
      */
     @Override
     protected void store(final WizardDescriptor wizard) {
-        CidsBean selectedMetaData = getComponent().getSelectedMetaData();
-        final CidsBean wizMetaData = (CidsBean)wizard.getProperty(MetaDataWizardAction.PROP_SELECTED_METADATA_BEAN);
+        // do nothing if the next button was not pressed.
+        if (WizardDescriptor.NEXT_OPTION.equals(wizard.getValue())) {
+            CidsBean selectedMetaData = getComponent().getSelectedMetaData();
+            final CidsBean wizMetaData = (CidsBean)wizard.getProperty(MetaDataWizardAction.PROP_SELECTED_METADATA_BEAN);
 
-        // the store method is always run twice. Check if it is the second execution.
-        // it is the second execution if:
-        // meta data in the wizard is not null
-        if (wizMetaData != null) {
-            return;
-        }
-
-        if (selectedMetaData == null) {
-            try {
-                // no metadata selected, thus create a new metadata and add it to the resource
-                selectedMetaData = CidsBean.createNewCidsBeanFromTableName("SWITCHON", "metadata"); // NOI18N
-                DefaultPropertySetter.setDefaultsToMetaDataCidsBean(selectedMetaData);
-
-                final CidsBean resource = (CidsBean)wizard.getProperty(MetaDataWizardAction.PROP_RESOURCE_BEAN);
-                resource.getBeanCollectionProperty("metadata").add(selectedMetaData); // NOI18N
-            } catch (Exception ex) {
-                LOG.error(ex, ex);
+            // the store method is always run twice. Check if it is the second execution.
+            // it is the second execution if:
+            // meta data in the wizard is not null
+            if (wizMetaData != null) {
                 return;
             }
-        }
 
-        wizard.putProperty(
-            MetaDataWizardAction.PROP_SELECTED_METADATA_BEAN,
-            selectedMetaData);
+            if (selectedMetaData == null) {
+                try {
+                    // no metadata selected, thus create a new metadata and add it to the resource
+                    selectedMetaData = CidsBean.createNewCidsBeanFromTableName("SWITCHON", "metadata"); // NOI18N
+                    DefaultPropertySetter.setDefaultsToMetaDataCidsBean(selectedMetaData);
+
+                    final CidsBean resource = (CidsBean)wizard.getProperty(MetaDataWizardAction.PROP_RESOURCE_BEAN);
+                    resource.getBeanCollectionProperty("metadata").add(selectedMetaData); // NOI18N
+                } catch (Exception ex) {
+                    LOG.error(ex, ex);
+                    return;
+                }
+            }
+
+            wizard.putProperty(
+                MetaDataWizardAction.PROP_SELECTED_METADATA_BEAN,
+                selectedMetaData);
+        }
     }
 
     /**
--- src/main/java/de/cismet/cids/custom/switchon/wizards/panels/RepresentationsPanel.java
+++ src/main/java/de/cismet/cids/custom/switchon/wizards/panels/RepresentationsPanel.java
@@ -78,34 +78,37 @@ public class RepresentationsPanel extends GenericAbstractWizardPanel<Representat
      */
     @Override
     protected void store(final WizardDescriptor wizard) {
-        CidsBean selectedRepresentation = getComponent().getSelectedRepresentation();
-        final CidsBean wizMetaData = (CidsBean)wizard.getProperty(
-                MetaDataWizardAction.PROP_SELECTED_REPRESENTATION_BEAN);
+        // do nothing if the next button was not pressed.
+        if (WizardDescriptor.NEXT_OPTION.equals(wizard.getValue())) {
+            CidsBean selectedRepresentation = getComponent().getSelectedRepresentation();
+            final CidsBean wizMetaData = (CidsBean)wizard.getProperty(
+                    MetaDataWizardAction.PROP_SELECTED_REPRESENTATION_BEAN);
 
-        // the store method is always run twice. Check if it is the second execution.
-        // it is the second execution if:
-        // representation in the wizard is not null
-        if (wizMetaData != null) {
-            return;
-        }
-
-        if (selectedRepresentation == null) {
-            try {
-                // no representation selected, thus create a new representation and add it to the resource
-                selectedRepresentation = CidsBean.createNewCidsBeanFromTableName("SWITCHON", "representation"); // NOI18N
-                DefaultPropertySetter.setDefaultsToRepresentationCidsBean(selectedRepresentation);
-
-                final CidsBean resource = (CidsBean)wizard.getProperty(MetaDataWizardAction.PROP_RESOURCE_BEAN);
-                resource.getBeanCollectionProperty("representation").add(selectedRepresentation); // NOI18N
-            } catch (Exception ex) {
-                LOG.error(ex, ex);
+            // the store method is always run twice. Check if it is the second execution.
+            // it is the second execution if:
+            // representation in the wizard is not null
+            if (wizMetaData != null) {
                 return;
             }
-        }
 
-        wizard.putProperty(
-            MetaDataWizardAction.PROP_SELECTED_REPRESENTATION_BEAN,
-            selectedRepresentation);
+            if (selectedRepresentation == null) {
+                try {
+                    // no representation selected, thus create a new representation and add it to the resource
+                    selectedRepresentation = CidsBean.createNewCidsBeanFromTableName("SWITCHON", "representation"); // NOI18N
+                    DefaultPropertySetter.setDefaultsToRepresentationCidsBean(selectedRepresentation);
+
+                    final CidsBean resource = (CidsBean)wizard.getProperty(MetaDataWizardAction.PROP_RESOURCE_BEAN);
+                    resource.getBeanCollectionProperty("representation").add(selectedRepresentation); // NOI18N
+                } catch (Exception ex) {
+                    LOG.error(ex, ex);
+                    return;
+                }
+            }
+
+            wizard.putProperty(
+                MetaDataWizardAction.PROP_SELECTED_REPRESENTATION_BEAN,
+                selectedRepresentation);
+        }
     }
 
     @Override
