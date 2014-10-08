switchonproject/cids-custom-switchon

commit 7382bf6bc262491d1ad609ace5b0c9d73a996d49

#92 simplified PropertyChangeListener of wizard

src/main/java/de/cismet/cids/custom/switchon/wizards/MetaDataWizardAction.java
@@ -125,21 +125,19 @@ public class MetaDataWizardAction extends AbstractAction implements CidsClientTo
                                 || WizardDescriptor.FINISH_OPTION.equals(wizard.getValue())) {
                         MetaDataWizardAction.this.setEnabled(true);
                     }
-                    if (evt.getNewValue() != null) {
-                        if ("org.openide.WizardDescriptor.FinishAction".equals(
-                                        evt.getNewValue().getClass().getCanonicalName())) {
-                            // persist the resource bean, when the wizard finished
-                            new CidsBeanPersistWorker(wizard).execute();
-                        } else if ((evt.getPropertyName() != null)
-                                    && evt.getPropertyName().equals(PROP_CONFIGURATION)) {
-                            // set the defaults based on the chosen profile
-                            if (evt.getNewValue().equals("basic")) {
-                                setBasicDefaults(wizard);
-                            } else if (evt.getNewValue().equals("advanced")) {
-                                setAdvancedDefaults(wizard);
-                            } else if (evt.getNewValue().equals("expert")) {
-                                setExpertDefaults(wizard);
-                            }
+                    if (WizardDescriptor.FINISH_OPTION.equals(wizard.getValue())) {
+                        // persist the resource bean, when the wizard finished
+                        new CidsBeanPersistWorker(wizard).execute();
+                    }
+                    if (PROP_CONFIGURATION.equals(evt.getPropertyName())) {
+                        final Object newValue = evt.getNewValue();
+                        // set the defaults based on the chosen profile
+                        if ("basic".equals(newValue)) {
+                            setBasicDefaults(wizard);
+                        } else if ("advanced".equals(newValue)) {
+                            setAdvancedDefaults(wizard);
+                        } else if ("expert".equals(newValue)) {
+                            setExpertDefaults(wizard);
                         }
                     }
                 }

commit 123aade251539522302dc9538ae60b9c0e176743

Fixes #102: check for WizardDescriptor
This bug was introduced in 7382bf6. In that commit the
PropertyChangeListener of the wizard, checked if the wizard was finished
with evt.getNewValue() == FINISH_OPTION. The problem is that such an
event is fired twice.
Therefore it has to be checked if evt.getNewValue() is an instance of
the class WizardDescriptor.FinishAction. Although the keyword instanceof
can not be used as that class is an inner class of WizardDescriptor an
has the default access modifier.

src/main/java/de/cismet/cids/custom/switchon/wizards/MetaDataWizardAction.java
@@ -125,8 +125,12 @@ public class MetaDataWizardAction extends AbstractAction implements CidsClientTo
                                 || WizardDescriptor.FINISH_OPTION.equals(wizard.getValue())) {
                         MetaDataWizardAction.this.setEnabled(true);
                     }
-                    if (WizardDescriptor.FINISH_OPTION.equals(wizard.getValue())) {
-                        // persist the resource bean, when the wizard finished
+                    // WizardDescriptor.FINISH_OPTION.equals(wizard.getValue()) can not be used, as such an event is
+                    // fired twice
+                    if ((evt.getNewValue() != null)
+                                && "org.openide.WizardDescriptor.FinishAction".equals(
+                                    evt.getNewValue().getClass().getCanonicalName())) {
+                        // persist the resource bean, when the wizard is finished
                         new CidsBeanPersistWorker(wizard).execute();
                     }
                     if (PROP_CONFIGURATION.equals(evt.getPropertyName())) {